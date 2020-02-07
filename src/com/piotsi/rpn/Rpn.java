1e2d5b4b9e742fdc02e2291f3ef87281b24815ea
piotsik <36536181+piotsik@users.noreply.github.com>
2020-02-07 15:28:48 +0100

    Added character distinction

--- src/com/piotsi/rpn/Rpn.java
+++ src/com/piotsi/rpn/Rpn.java
@@ -1,7 +1,31 @@
 package com.piotsi.rpn;
 
+import java.util.Stack;
+
 public class Rpn {
+
+    static boolean isNumeric(String s){
+        try {
+            Integer.parseInt(s);
+            return true;
+        }
+        catch (NumberFormatException nfe) {
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
+        String expression = "15 7 1 1 + − ÷ 3 × 2 1 1 + + −";
+        Stack<String> e = new Stack<String>();
+
+        for (String val: expression.split(" ")){
+            e.push(val);
+        }
+        System.out.println(e.size());
+        for (Object val: e){
+            System.out.println(val);
+        }
+
 
     }
 }
